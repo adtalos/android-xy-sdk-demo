38035b3a9a5b30af7c731082b98145bc0b2a5c48
andot <mabingyao@gmail.com>
2019-11-07 20:10:28 +0800

    完成演示实例

---
+++ app/src/main/java/com/xy/demo/BannerActivity.java
@@ -0,0 +1,54 @@
+package com.xy.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class BannerActivity extends AppCompatActivity {
+
+    private com.xy.sdk.View topBannerView;
+    private com.xy.sdk.View bottomBannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_banner);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // 获取横幅广告
+        topBannerView = findViewById(R.id.top_banner_view);
+        // 设置定时轮转模式开启
+        topBannerView.setCarouselModeEnabled(true);
+        // 设置轮转动画开启
+        topBannerView.setAnimationEnabled(true);
+        // 设置事件
+        topBannerView.setListener(DemoListener.instance);
+
+        bottomBannerView = findViewById(R.id.bottom_banner_view);
+        bottomBannerView.setListener(DemoListener.instance);
+        // 手动加载广告
+        bottomBannerView.load("96753DCF925E8DC7C105B3D3ED1138EA");
+    }
+
+    @Override
+    protected void onDestroy() {
+        topBannerView.destroy();
+        bottomBannerView.destroy();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        topBannerView.pause();
+        bottomBannerView.pause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        topBannerView.resume();
+        bottomBannerView.resume();
+        super.onResume();
+    }
+}
---
+++ app/src/main/java/com/xy/demo/DemoListener.java
@@ -0,0 +1,107 @@
+package com.xy.demo;
+
+import android.util.Log;
+
+import com.xy.sdk.DefaultCustomListener;
+import com.xy.sdk.Listener;
+import com.xy.sdk.VideoController;
+import com.xy.sdk.VideoListener;
+
+public class DemoListener implements Listener, VideoListener, DefaultCustomListener {
+    public static final DemoListener instance = new DemoListener();
+
+    @Override
+    public void onRendered() {
+        Log.d("DemoListener", "onRendered");
+    }
+
+    @Override
+    public void onImpressionFinished() {
+        Log.d("DemoListener", "onImpressionFinished");
+    }
+
+    @Override
+    public void onImpressionReceivedError(int errorCode, String description) {
+        Log.d("DemoListener", "onImpressionReceivedError: " + errorCode + ":" + description);
+    }
+
+    @Override
+    public void onLoaded() {
+        Log.d("DemoListener", "onLoaded");
+    }
+
+    @Override
+    public void onFailedToLoad(Exception e) {
+        Log.d("DemoListener", "onFailedToLoad:" + Log.getStackTraceString(e));
+    }
+
+    @Override
+    public void onOpened() {
+        Log.d("DemoListener", "onOpened");
+    }
+
+    @Override
+    public void onClicked() {
+        Log.d("DemoListener", "onClicked");
+    }
+
+    @Override
+    public void onLeftApplication() {
+        Log.d("DemoListener", "onLeftApplication");
+    }
+
+    @Override
+    public void onClosed() {
+        Log.d("DemoListener", "onClosed");
+    }
+
+    @Override
+    public void onVideoLoad(VideoController.Metadata metadata) {
+        Log.d("DemoListener", "VideoLoad: " + metadata.getDuration());
+    }
+
+    @Override
+    public void onVideoStart() {
+        Log.d("DemoListener", "onVideoStart");
+    }
+
+    @Override
+    public void onVideoPlay() {
+        Log.d("DemoListener", "onVideoPlay");
+    }
+
+    @Override
+    public void onVideoPause() {
+        Log.d("DemoListener", "onVideoPause");
+    }
+
+    @Override
+    public void onVideoEnd() {
+        Log.d("DemoListener", "onVideoEnd");
+    }
+
+    @Override
+    public void onVideoVolumeChange(double volume, boolean muted) {
+        Log.d("DemoListener", "onVideoVolumeChange: " + volume + ", " + (muted ? "muted" : "unmuted"));
+    }
+
+    @Override
+    public void onVideoTimeUpdate(double currentTime, double duration) {
+        Log.d("DemoListener", "onVideoTimeUpdate: " + (currentTime / duration) * 100 + "%");
+    }
+
+    @Override
+    public void onVideoError() {
+        Log.d("DemoListener", "onVideoError");
+    }
+
+    @Override
+    public void onVideoBreak() {
+        Log.d("DemoListener", "onVideoBreak");
+    }
+
+    @Override
+    public void on(String name, String value) {
+        Log.d("DemoListener", "onCustomEvent: " + name + ", value: " + value);
+    }
+}
