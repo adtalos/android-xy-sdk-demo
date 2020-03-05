cfe6f5c59454a0d06f1021520a36c5d8937d653a
andot <mabingyao@gmail.com>
2020-03-05 15:45:04 +0800

    更新 Demo，避免误导用户把开屏放在 MainActivity 里面。

--- app/src/main/java/com/xy/demo/DemoAppliaction.java
+++ app/src/main/java/com/xy/demo/DemoAppliaction.java
@@ -1,11 +1,17 @@
 package com.xy.demo;
 
 import android.app.Application;
+import android.content.pm.ActivityInfo;
 
 import com.bun.miitmdid.core.JLibrary;
+import com.xy.sdk.Controller;
+import com.xy.sdk.LandingPageActivity;
+import com.xy.sdk.SDK;
 
 public class DemoAppliaction extends Application {
     private final MsaHelper helper = new MsaHelper();
+    // 开屏广告
+    private final Controller splash = new Controller("5C3DD65A809B08A2D6CF3DEFBC7E09C7", Controller.SPLASH);
 
     @Override
     public void onCreate() {
@@ -13,6 +19,26 @@ public class DemoAppliaction extends Application {
         try {
             JLibrary.InitEntry(this.getApplicationContext());
             helper.getDeviceIds(this.getApplicationContext());
+
+            // 设置广告落地页显示导航按钮工具栏
+            LandingPageActivity.setDisplayActionBarEnabled(true);
+
+            // 设置广告落地页是否使用换页动画效果
+            LandingPageActivity.setAnimationEnabled(true);
+
+            // 设置广告落地页是否全屏显示
+            LandingPageActivity.setFullScreenEnabled(true);
+
+            // 设置广告落地页显示方向
+            LandingPageActivity.setOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            // 设置开屏广告事件（非必须）
+            splash.setListener(DemoListener.instance);
+            splash.setVideoListener(DemoListener.instance);
+            splash.setDefaultCustomListener(DemoListener.instance);
+
+            // 显示开屏广告
+            splash.show();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
--- app/src/main/java/com/xy/demo/MainActivity.java
+++ app/src/main/java/com/xy/demo/MainActivity.java
@@ -13,8 +13,6 @@ import com.xy.sdk.SDK;
 
 public class MainActivity extends AppCompatActivity {
 
-    // 开屏广告
-    private final Controller splash = new Controller("5C3DD65A809B08A2D6CF3DEFBC7E09C7", Controller.SPLASH);
     // 插屏广告
     private final Controller interstitial = new Controller("2EF810225D10260506CBB704C96C5325", Controller.INTERSTITIAL);
     // 激励视频广告
@@ -26,26 +24,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // 请求广告 SDK 需要的权限
         SDK.requestPermissions(this);
-
-        // 设置广告落地页显示导航按钮工具栏
-        LandingPageActivity.setDisplayActionBarEnabled(true);
-
-        // 设置广告落地页是否使用换页动画效果
-        LandingPageActivity.setAnimationEnabled(true);
-
-        // 设置广告落地页是否全屏显示
-        LandingPageActivity.setFullScreenEnabled(true);
-
-        // 设置广告落地页显示方向
-        LandingPageActivity.setOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
-        // 设置开屏广告事件（非必须）
-        splash.setListener(DemoListener.instance);
-        splash.setVideoListener(DemoListener.instance);
-        splash.setDefaultCustomListener(DemoListener.instance);
-
-        // 显示开屏广告
-        splash.show();
 
         Button bannerTestButton = findViewById(R.id.banner_test_button);
         bannerTestButton.setOnClickListener(new View.OnClickListener() {
