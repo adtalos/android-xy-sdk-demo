dad66e7077a3b4d3ed071f43a38641cf4d43b765
andot <mabingyao@gmail.com>
2023-10-11 18:19:32 +0800

    update sdk to 2.7.0

--- app/src/main/java/com/xy/demo/MainActivity.java
+++ app/src/main/java/com/xy/demo/MainActivity.java
@@ -2,12 +2,12 @@ package com.xy.demo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.xy.sdk.Controller;
-import com.xy.sdk.LandingPageActivity;
 import com.xy.sdk.SDK;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,8 +21,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // 下面注释的语句仅用于演示请求权限
-        // SDK.requestPermissions(this);
+        // 请求权限
+        SDK.requestPermissions(this);
 
         Button bannerTestButton = findViewById(R.id.banner_test_button);
         bannerTestButton.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/xy/demo/NativeActivity.java
+++ app/src/main/java/com/xy/demo/NativeActivity.java
@@ -1,6 +1,7 @@
 package com.xy.demo;
 
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
