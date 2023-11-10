044f911f98a59c2d4070edd448f470d4cc93a9e9
andot <mabingyao@gmail.com>
2023-11-10 12:19:22 +0800

    增加区域、用户、隐私设置示例

--- app/src/main/java/com/xy/demo/DemoAppliaction.java
+++ app/src/main/java/com/xy/demo/DemoAppliaction.java
@@ -3,8 +3,14 @@ package com.xy.demo;
 import android.app.Application;
 import android.content.pm.ActivityInfo;
 
+import com.xy.sdk.Area;
 import com.xy.sdk.Controller;
+import com.xy.sdk.Gender;
 import com.xy.sdk.LandingPageActivity;
+import com.xy.sdk.Privacy;
+import com.xy.sdk.PrivacyStatus;
+import com.xy.sdk.SDK;
+import com.xy.sdk.User;
 
 public class DemoAppliaction extends Application {
     // 开屏广告
@@ -14,6 +20,16 @@ public class DemoAppliaction extends Application {
     public void onCreate() {
         super.onCreate();
         try {
+            SDK.setArea(Area.CHINA);
+            SDK.setUser(User.newBuilder()
+                    .setId("demo_id")
+                    .setAge(18)
+                    .setGender(Gender.MALE)
+                    .setKeywords("demo", "test")
+                    .build());
+            SDK.setPrivacy(Privacy.newBuilder()
+                    .setPersonalizedRecommendation(PrivacyStatus.AUTHORIZED)
+                    .build());
             // 设置广告落地页显示导航按钮工具栏
             LandingPageActivity.setDisplayActionBarEnabled(true);
 
